6aeeae1f2bafe465e113750ce6449eb32fbb3ba8
RafasyaSubhan <rafasya.subhan@gmail.com>
2025-08-27 14:16:53 +0700

    Menambahkan allowed host

--- football_news/settings.py
+++ football_news/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-8_hctm5_=j&v*59fg@j^h-*=o+)+1#tyjlma#i77izb)hpzcdf
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rafasya-muhammad-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
